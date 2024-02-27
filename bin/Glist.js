a7d7abe74eaedccf6bb4635be4a3d5e66d97507e
Renato Alcantara <renatoalcantara2022@gmail.com>
2024-02-27 12:37:21 -0300

    add git-list command

--- bin/Glist.js
+++ bin/Glist.js
@@ -1,16 +1,10 @@
 #!/usr/bin/env node
 
 import { logger } from "../lib/logger.js"
-import { exec } from "node:child_process"
+import { spawn } from "node:child_process"
 
 try {
-    exec('ssh-add -l', (error, stdout, stderr) => {
-        if (error) {
-            throw new Error(error)
-        }
-        logger.info(stdout)
-        logger.info(stderr)
-    })
+    spawn('ssh-add', ['-l'], { shell: true })
 } catch (e) {
     logger.error(e)
 }
